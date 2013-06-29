c6e1fe2142ef29b5ebe408dd9952aa464e098f85
Tomas Perez <tom@0x101.com>
2013-06-29 16:09:46 +0200

    Adding base files for #4.

---
+++ jsperf/localstorage/README.md
@@ -0,0 +1,11 @@
+Local storage notification delay
+================================
+
+Overview
+--------
+[SpainJS](http://spainjs.org/)
+
+We want to analyze the time to receive the onstorage notification across tabs, between different browsers.
+
+Comments
+--------
---
+++ jsperf/localstorage/core.js
@@ -0,0 +1,14 @@
+var Spotify = (Spotify || {});
+
+/**
+ * @class {Spotify.Example.LocalStorage}
+ * @author Tomas Perez <tom@0x101.com>
+ */
+Spotify.Example = (function(namespace) {
+
+    namespace.LocalStorage = {
+    };
+
+  return namespace;
+
+})(Spotify.Example || {});
